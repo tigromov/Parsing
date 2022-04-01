a34318b2996e386edf27fb55e40cd680a60e95f0
tigromov <102591826+tigromov@users.noreply.github.com>
2022-04-01 17:05:08 +0600

    Working parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,29 +2,75 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, NullPointerException {
+        int time = 1000;
+        int counter = 0;
+        int li = 2;
 
         System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://kaspi.kz/shop/p/luther-lcg-0-chernyi-100388079/?c=750000000");
-        Document document = Jsoup.parse(webDriver.getPageSource());
+        webDriver.get("https://kaspi.kz/shop/search/?text=music%20park&q=:allMerchants:Musicpark");
 
-        Elements els = document.getElementsByClass("sellers-table__price-cell-text");
-        Element els2 = els.first();
-        System.out.println(els2.text());
+
+       ///close Choose your City
+        WebElement btnClose = webDriver.findElement(By.xpath("//*[@id=\"dialogService\"]/div/div[1]/div[2]/i"));
+        btnClose.click(); Thread.sleep(time);
+        ///
+
+
+
+        for (int i = 1; i < 30; i++) {
+           webDriver.get("https://kaspi.kz/shop/search/?text=music%20park&q=:allMerchants:Musicpark&page=" + i +"");
+           Thread.sleep(time + 3000);
+          //  if (counter>3)li = 7;
+            //WebElement btn = webDriver.findElement(By.xpath("//*[@id=\"page\"]/div[3]/div/div/div[2]/div[2]/div[2]/li[" + li + "]"));
+           // btn.click();
+           // Thread.sleep(time+3000);
+
+            Document documentGlobal = Jsoup.parse(webDriver.getPageSource());
+            Elements wrappers = documentGlobal.getElementsByClass("item-card__image-wrapper");
+
+
+            for (Element wr : wrappers) {
+
+                counter++;
+                webDriver.get(wr.attr("href"));
+                Thread.sleep(time);
+                Document document2 = Jsoup.parse(webDriver.getPageSource());
+                Elements els = document2.getElementsByClass("sellers-table__price-cell-text");
+                Element els2 = els.first();
+                String name = document2.getElementsByClass("item__heading").text();
+                try{
+
+                System.out.println(counter + " " + name + " " + els2.text());
+                Thread.sleep(time);} catch (NullPointerException e) {
+                    System.out.println(counter + " " + name + " Нет актуальных предложений по данному товару");
+                }
+
+            }
+
+
+           // webDriver.getPageSource();
+
+        }
+
         webDriver.close();
-        //Document doc = Jsoup.connect("https://kaspi.kz/shop/p/akg-k-52-chernyi-4802475/?c=750000000").get();
+
+      //  Elements els = document.getElementsByClass("sellers-table__price-cell-text");
+//        Element els2 = els.first();
+//        System.out.println(els2.text());
+        webDriver.close();
 
 
-      //  System.out.println(doc.getElementsByTag("div"));
 
-       // System.out.println(doc.getElementsByTag("body"));
 
 
     }
