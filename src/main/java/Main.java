4cbd2bdbb4de35e7e591cf279704989610854a63
tigromov <102591826+tigromov@users.noreply.github.com>
2022-04-05 18:56:09 +0600

    Working parser + making xl file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,10 +10,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, NullPointerException {
+
+        Workbook midWb = new XSSFWorkbook();
+        Sheet midSheet = midWb.createSheet();
+        FileOutputStream fos = new FileOutputStream("C:/Rest/ParsingMid.xls");
+
         int time = 1000;
         int counter = 0;
         int li = 2;
@@ -27,13 +36,10 @@ public class Main {
 
 
 
-        for (int i = 1; i < 30; i++) {
+        for (int i = 1; i < 23; i++) {
            webDriver.get("https://kaspi.kz/shop/search/?text=music%20park&q=:allMerchants:Musicpark&page=" + i +"");
            Thread.sleep(time + 3000);
-          //  if (counter>3)li = 7;
-            //WebElement btn = webDriver.findElement(By.xpath("//*[@id=\"page\"]/div[3]/div/div/div[2]/div[2]/div[2]/li[" + li + "]"));
-           // btn.click();
-           // Thread.sleep(time+3000);
+
 
             Document documentGlobal = Jsoup.parse(webDriver.getPageSource());
             Elements wrappers = documentGlobal.getElementsByClass("item-card__image-wrapper");
@@ -49,27 +55,27 @@ public class Main {
                 Element els2 = els.first();
                 String name = document2.getElementsByClass("item__heading").text();
                 String sku = document2.getElementsByClass("item__sku").text().substring(12);
+                String price = els2.text();
 
                 try{
 
-                System.out.println(counter + " " + name + " " + els2.text() + " артикул каспи: " + sku);
+                System.out.println(counter + " " + name + " " + price + " артикул каспи: " + sku);
                 Thread.sleep(time);} catch (NullPointerException e) {
                     System.out.println(counter + " " + name + " Нет актуальных предложений по данному товару");
                 }
+                midSheet.createRow(counter).createCell(0).setCellValue(name);
+                midSheet.getRow(counter).createCell(1).setCellValue(price);
+                midSheet.getRow(counter).createCell(2).setCellValue(sku);
+
 
             }
-
-
-           // webDriver.getPageSource();
-
         }
 
         webDriver.close();
+        midWb.write(fos);
+        fos.close();
 
-      //  Elements els = document.getElementsByClass("sellers-table__price-cell-text");
-//        Element els2 = els.first();
-//        System.out.println(els2.text());
-        webDriver.close();
+
 
 
 
